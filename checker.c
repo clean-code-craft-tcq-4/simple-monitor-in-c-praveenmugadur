1d13f424cf80d777afff38bd38a7e592b63819bf
praveenmugadur <praveenmugadur5@gmail.com>
2022-07-08 17:31:51 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include<stdbool.h>  
 #include <assert.h>
 #include "Bms.h"
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
 
-  boolean tempVal = checkTemp(temperature);
-  boolean socVal = checkSoc(soc);
-  boolean chargeRateVal = checkchargeRate(chargeRate);
+  bool tempVal = checkTemp(temperature);
+  bool socVal = checkSoc(soc);
+  bool chargeRateVal = checkchargeRate(chargeRate);
   
   return (tempVal && socVal && chargeRateVal);
 }
