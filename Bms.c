2742aa5194fda614ff0d8369538188d699d31ac0
praveenmugadur <praveenmugadur5@gmail.com>
2022-07-08 11:12:13 +0530

    Update Bms.c

--- Bms.c
+++ Bms.c
@@ -5,8 +5,8 @@ int batteryCheck(float value, float min, float max)
 {
     if (value < min || value > max)
     {
-        return NOT_OK;
+        return Check_OK;
     }
 
-    return IS_OK;
+    return Check_NOT_OK;
 }
