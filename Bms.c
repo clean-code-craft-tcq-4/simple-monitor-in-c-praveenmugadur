55d4e4d8d8bc8e35c23c37f48683eec9c22fd622
praveenmugadur <praveenmugadur5@gmail.com>
2022-07-08 11:09:21 +0530

    Update Bms.c

--- Bms.c
+++ Bms.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
+#include "Bms.h"
 
+int batteryCheck(float value, float min, float max)
+{
+    if (value < min || value > max)
+    {
+        return NOT_OK;
+    }
+
+    return IS_OK;
+}
