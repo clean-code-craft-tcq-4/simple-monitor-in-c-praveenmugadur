a90609b44f03ec2ce0ff942988e4fa35593319cb
praveenmugadur <praveenmugadur5@gmail.com>
2022-07-08 17:26:24 +0530

    Update Bms.c

--- Bms.c
+++ Bms.c
@@ -10,3 +10,39 @@ int batteryCheck(float value, float min, float max)
 
     return Check_OK;
 }
+
+bool checkTemp(float temperature)
+{
+   int val = batteryCheck(temperature, 0, 45);
+    if(val == Check_NOT_OK){
+         printf("Temperature out of range!\n");
+        return Check_NOT_OK;
+    }
+    else {
+         return Check_OK;
+    }      
+}
+
+bool checkSoc(float soc)
+{
+   int val = batteryCheck(soc, 20, 80);
+    if(val == Check_NOT_OK){
+         printf("State of Charge out of range!\n");
+        return Check_NOT_OK;
+    }
+    else {
+         return Check_OK;
+    }      
+}
+
+bool checkchargeRate(float chargeRate)
+{
+   int val = batteryCheck(chargeRate, 0, 0.8);
+    if(val == Check_NOT_OK){
+         printf("State of Charge out of range!\n");
+        return Check_NOT_OK;
+    }
+    else {
+         return Check_OK;
+    }      
+}
