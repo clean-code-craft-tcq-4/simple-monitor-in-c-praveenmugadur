387b66452694449663f24f7572c72eab31df783b
praveenmugadur <praveenmugadur5@gmail.com>
2022-07-13 12:09:27 +0530

    Update Bms.h

--- Bms.h
+++ Bms.h
@@ -1,5 +1,7 @@
 #define Check_OK 1
 #define Check_NOT_OK 0 
 
-int 
-batteryCheck(float value, float min, float max);
+int batteryCheck(float value, float min, float max);
+int checkTemp(float temperature);
+int checkSoc(float soc);
+int checkchargeRate(float chargeRate);
