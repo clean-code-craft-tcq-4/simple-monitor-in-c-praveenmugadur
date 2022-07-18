d6f14aedbc3746e36db2ab1d0dd5de6f3ede24c5
praveenmugadur <praveenmugadur5@gmail.com>
2022-07-18 14:33:32 +0530

    Create TestBatteryStatus.h

---
+++ TestBatteryStatus.h
@@ -0,0 +1,12 @@
+#ifndef TEST_BMSSTATUS_H
+#define TEST_BMSSTATUS_H
+
+#include <assert.h>
+#include "Bms.h"
+
+extern int BatteryTestIsOk(float Soc, float Temp , float ChargeRate, char TempUnit)
+extern int BatteryTestIsNotOk(float Soc, float Temp , float ChargeRate, char TempUnit)
+void BatteryStatusTest(void);
+
+
+#endif
