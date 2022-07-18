2074f15ecaf471d7e4d0916f6b2bda188f5121e9
praveenmugadur <praveenmugadur5@gmail.com>
2022-07-18 17:02:49 +0530

    Update Bms.c

--- Bms.c
+++ Bms.c
@@ -42,6 +42,71 @@ int checkSocOk(float soc )
 	return socStatus;
 }
 
+
+
+
+/****************TEMP*****************************************/
+int BatteryTempLowLimit(float temp)
+{	
+	if ((temp >= MIN_LOWTEMPBREACH) || (temp <= MIN_TEMPNORMAL))
+	{
+		return E_NOT_OK; 
+	}
+	else
+	{
+		return E_OK; 
+	}
+}
+
+int BatteryTempHighLimit(float temp, char tempUnit)
+{
+	if ((temp >= MIN_HIGHTEMPWARNING) || (temp <= MIN_HIGHTEMPBREACH))
+	{
+		return E_NOT_OK; 
+	}
+	else 
+	{
+		inttBattConvertTemp(temp, tempUnit);
+		return E_NOT_OK; 
+	}
+	
+}
+
+int checkTempOK(float temp, char tempUnit )
+{
+	int tempStatus = E_OK;
+	if((temp >= MIN_LOWTEMPBREACH) && (temp < MIN_HIGHTEMPWARNING))
+	{
+		tempStatus = BattTempLowLimit(temp);
+	}
+	else
+	{
+		tempStatus = BattTempHighLimit(temp, tempUnit);
+	}
+	return tempStatus;
+}
+
+ bool BatteryTempUnit(char cel)
+ {
+ 	if(cel == TEMP_UNIT_CELCIUS)
+		return true;
+	 else
+		return false;
+ }
+
+float BatteryConvertTemp(float temp, char tempUnit)
+{
+	float celcius;
+	if((temp > MAX_HIGHTEMPBREACH) && (tempUnit == TEMP_UNIT_FARENHEIT))
+	{
+		celcius = (temp - 32) * 5 / 9;
+	}
+	else
+	{
+		return temp;
+	}
+}
+
 int batteryCheck( float soc, float temp , float chargeRate, char tempUnit )
 {	
 	float stateOfCharge = checkSocOk(soc);
